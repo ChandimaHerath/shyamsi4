5073e0298af6b8f23913000d146fac79f836f126
chandimaherath <chandimaherath@hotmail.com>
2021-02-05 11:00:01 +0530

    component vana created

---
+++ shymsi4/src/app/modules/vana/vana.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vana',
+  templateUrl: './vana.component.html',
+  styleUrls: ['./vana.component.scss']
+})
+export class VanaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
