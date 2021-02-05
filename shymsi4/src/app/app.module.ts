56211e632dccc58b290a79b60d761b3502b4c8fa
chandimaherath <chandimaherath@hotmail.com>
2021-02-05 09:26:27 +0530

    component chathura created

--- shymsi4/src/app/app.module.ts
+++ shymsi4/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChandimaComponent } from './modules/chandima/chandima.component';
+import { ChathuraComponent } from './modules/chathura/chathura.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChandimaComponent
+    ChandimaComponent,
+    ChathuraComponent
   ],
   imports: [
     BrowserModule,
---
+++ shymsi4/src/app/modules/chathura/chathura.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chathura',
+  templateUrl: './chathura.component.html',
+  styleUrls: ['./chathura.component.scss']
+})
+export class ChathuraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
