43d8de1c9abf891b05d3a8632d439f26129b4e92
chandimaherath <chandimaherath@hotmail.com>
2021-02-05 10:34:49 +0530

    Revert "Merge branch 'UI/Chathura' of https://github.com/ChandimaHerath/shyamsi4 into UI/Chathura"
    This reverts commit 4be9f90b2a8a2e0c605359ef1a55ebcaa4f53500.

--- shymsi4/src/app/app.module.ts
+++ shymsi4/src/app/app.module.ts
@@ -6,15 +6,13 @@ import { AppComponent } from './app.component';
 import { ChandimaComponent } from './modules/chandima/chandima.component';
 import { ChathuraComponent } from './modules/chathura/chathura.component';
 import { Chathura2Component } from './modules/chathura2/chathura2.component';
-import { Chandima2Component } from './modules/chandima2/chandima2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChandimaComponent,
     ChathuraComponent,
-    Chathura2Component,
-    Chandima2Component
+    Chathura2Component
   ],
   imports: [
     BrowserModule,
